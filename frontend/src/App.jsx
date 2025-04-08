c9397cab2d3aee948252545bb26f92a51e2fbe00
htetsoemoe <soemoehtet.mdy@gmail.com>
2025-04-08 10:36:15 +0630

    tailwind css setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,28 +7,19 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="
+         flex items-center justify-center 
+         mx-auto h-[100vh] rounded-2xl
+         bg-gray-500
+    ">
+      <div className="
+        rounded-2xl
+        text-5xl bg-amber-200
+        p-10
+      ">
+        Hello from chat app with socket.io
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
