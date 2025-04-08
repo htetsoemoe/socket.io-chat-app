73d0335c932f4c26b267026d4d59ca503fe8eed1
htetsoemoe <soemoehtet.mdy@gmail.com>
2025-04-08 11:17:02 +0630

    daisyUI setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,19 +7,47 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="
-         flex items-center justify-center 
-         mx-auto h-[100vh] rounded-2xl
-         bg-gray-500
-    ">
+    <>
       <div className="
-        rounded-2xl
-        text-5xl bg-amber-200
-        p-10
-      ">
-        Hello from chat app with socket.io
+         flex flex-col gap-5 items-center 
+         mx-auto  rounded-2xl
+         bg-gray-400 p-5
+        ">
+        <div className="
+            rounded-2xl
+            text-5xl bg-amber-200
+            p-10 mt-5
+          ">
+          Hello from chat app with socket.io
+        </div>
+        <div className='flex flex-col gap-7 mt-5'>
+          <div className="text-2xl font-light">Daisy UI</div>
+          <div className='flex justify-center gap-7'>
+            <button className="btn">Button</button>
+            <button className="btn btn-primary">Button</button>
+            <button className="btn btn-secondary">Button</button>
+            <button className="btn btn-accent">Button</button>
+            <button className="btn btn-link">Button</button>
+          </div>
+          <div className='mb-5'>
+            <div className="card bg-base-100 w-96 shadow-sm">
+              <figure>
+                <img
+                  src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                  alt="Shoes" />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">Card Title</h2>
+                <p>A card component has a figure, a body part</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Buy Now</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
