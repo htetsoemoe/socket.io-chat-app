1da5897938437af25077c7e951e739be56a57a4f
htetsoemoe <soemoehtet.mdy@gmail.com>
2025-04-25 11:39:46 +0630

    other users in sidebar

---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { getRandomEmoji } from "../../utils/emojis.js"
+import useGetConversations from '../../hooks/useGetConversations'
+import Conversation from './Conversation'
+
+const Conversations = () => {
+    const { conversations, loading } = useGetConversations()
+
+    return (
+        <div className='py-2 flex flex-col overflow-auto'>
+            {conversations.map((conversation, idx) => (
+                <Conversation
+                    key={conversation._id}
+                    conversation={conversation}
+                    lastIdx={idx === conversations.length - 1}
+                    emoji={getRandomEmoji()}
+                />
+            ))}
+
+        </div>
+    )
+}
+
+export default Conversations
