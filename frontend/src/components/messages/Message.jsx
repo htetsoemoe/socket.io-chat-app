7b2e51fd1a153acbcec8f7c7c699f954132edc34
htetsoemoe <soemoehtet.mdy@gmail.com>
2025-05-02 14:34:13 +0630

    heart icon in chat footer

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useAuthContext } from '../../context/AuthContext'
 import useConversation from '../../zustand/useConversation'
 import { extractTime } from '../../utils/extractTime.js'
+import { IoIosHeartEmpty, IoIosHeart } from "react-icons/io";
 
 const Message = ({ message }) => {
     const { authUser } = useAuthContext()
@@ -13,20 +14,41 @@ const Message = ({ message }) => {
     const bubbleBgColor = fromMe ? "bg-blue-500" : "bg-gray-700";
     const formattedTime = extractTime(message.createdAt);
 
-    const shakeClass = message?.shouldShake ? "shake" : "";
+    const handleHeartClick = () => {
+        console.log("heart clicked")
+    }
 
+    console.log(message)
     return (
-        <div className={`chat ${chatClassName}`}>
-            <div className="chat-image avatar">
-                <div className="w-10 rounded-full">
-                    <img
-                        alt='chat bubble component'
-                        src={profilePic}
-                    />
+        <div className=''>
+            <div className={`chat ${chatClassName}`}>
+                <div className="chat-image avatar">
+                    <div className="w-10 rounded-full">
+                        <img
+                            alt='chat bubble component'
+                            src={profilePic}
+                        />
+                    </div>
+                </div>
+                <div className={`chat-bubble text-white pb-2 ${bubbleBgColor}`}>{message.message}</div>
+                <div className='chat-footer opacity-90 text-sm flex gap-1 items-center '>
+                    <div className='flex justify-between items-center mt-1'>
+                        <div className='mr-3'>{formattedTime}</div>
+                        <div className=''>
+                            {message?.isLike ? (
+                                <IoIosHeart
+                                    className="text-xl cursor-pointer"
+                                />
+                            ) : (
+                                <IoIosHeartEmpty
+                                    className="text-xl cursor-pointer"
+                                    onClick={handleHeartClick}
+                                />
+                            )}
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className={`chat-bubble text-white pb-2 ${bubbleBgColor} ${shakeClass}`}>{message.message}</div>
-            <div className='chat-footer opacity-90 text-xs flex gap-1 items-center '>{formattedTime}</div>
         </div>
     )
 }
