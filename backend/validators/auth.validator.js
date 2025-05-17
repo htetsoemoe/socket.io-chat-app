531009ec9412cb3e8a116de89c08070990bb4c4b
htetsoemoe <soemoehtet.mdy@gmail.com>
2025-05-17 10:24:34 +0630

    api: send verification opt and verify email opt

--- backend/validators/auth.validator.js
+++ backend/validators/auth.validator.js
@@ -49,3 +49,14 @@ export const signinValidator = validate({
         }),
     }),
 })
+
+export const verifyEmailOTPValidator = validate({
+    body: Joi.object({
+        otp: Joi.string().pattern(/^[0-9]{6}$/).required().messages({
+            "any.required": "OTP is required",
+            "string.otp": "OTP must be a string",
+            "string.pattern.base": "OTP must be a 6 digit number",
+            "string.empty": "OTP is required",
+        })
+    }),
+});
